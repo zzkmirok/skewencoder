53be7bc7321bac346ba938d428cc1efb6cf842f7
Zhikun Zhang <zhikun.zhang@ltt.rwth-aachen.de>
2025-07-26 06:55:07 +0200

    add test for transform colvar key

--- test/test_state_detection.py
+++ test/test_state_detection.py
@@ -5,3 +5,13 @@ def test_transform_colvar_key():
     bond_type = transform_colvar_key(colvar_key=colvar_key, pattern = r"^(H)\d+([A-GI-Za-gi-z]+)\d+$")
     assert bond_type == "H-C"
 
+    # TODO: temporal compatibality test. Later "o3h11" should be extracted as capital "H-O"
+    colvar_key = "o4h3"
+    bond_type = transform_colvar_key(colvar_key=colvar_key)
+    assert bond_type == "h-o"
+    colvar_key = "o3h11"
+    bond_type = transform_colvar_key(colvar_key=colvar_key)
+    assert bond_type == "h-o"
+    colvar_key = "o3h10"
+    bond_type = transform_colvar_key(colvar_key=colvar_key)
+    assert bond_type == "h-o"
